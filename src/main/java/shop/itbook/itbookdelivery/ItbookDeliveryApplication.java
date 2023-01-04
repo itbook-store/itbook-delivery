3044053b0f05be4b0a3afe332af5d920a6d69501
dahye <sdh46@chosun.kr>
2023-01-04 09:31:39 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookdelivery/ItbookDeliveryApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItbookDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/itbook/itbookdelivery/ItbookDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
