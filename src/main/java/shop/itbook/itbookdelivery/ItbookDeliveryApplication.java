1ac3d34eb6c69092269b282826a3fa11b1d4f3e2
dahye <sdh46@chosun.kr>
2023-01-04 13:42:34 +0900

    [chore] : 개발환경설정 기본설정 완료(yml, config, dependency)

--- src/main/java/shop/itbook/itbookdelivery/ItbookDeliveryApplication.java
+++ src/main/java/shop/itbook/itbookdelivery/ItbookDeliveryApplication.java
@@ -2,8 +2,12 @@ package shop.itbook.itbookdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import shop.itbook.itbookdelivery.config.RedisConfig;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ItbookDeliveryApplication {
 
 	public static void main(String[] args) {
